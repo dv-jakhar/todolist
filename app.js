5307b60fa75927a940ed061103009936935c0d48
dvjakhar <dharmveerjakhar.civ18@itbhu.ac.in>
2020-05-18 21:59:14 +0530

    updated app.js

--- app.js
+++ app.js
@@ -106,7 +106,12 @@ app.get("/work", function(req,res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+let port = process.env.PORT;
+if(port == "" || port==null)
+{
+  port=3000;
+}
 
-app.listen(3000 || process.env.PORT, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port "+ port);
 });
